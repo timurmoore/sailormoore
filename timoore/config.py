2499656ab262c7a98f81b404d4c48b689892c4a2
Timur Moore <timur.abdullin@clerk.com>
2020-08-16 03:23:39 +0300

    Create config.py

---
+++ timoore/config.py
@@ -0,0 +1,27 @@
+##############################
+# Please edit this line to "not_sample_anymore" to make the bot run
+is_example_config_or_not: "not_sample_anymore"
+##############################
+bot_token: "1360342490:AAGdgfXPzMzz4lSySb0qtnCb6cpS24Dt1C8"
+api_key: '1360037'
+api_hash: '4d0f34681d156cb0e59427e61a98448f'
+owner_id: '71265226'
+owner_username: "timurmoore"
+message_dump: "-1001454240722"
+gban_dump: "-1001454240722"
+load: []
+no_load: []
+strict_antispam: False
+workers: 4
+deepfry_token: None
+del_cmds: False
+sw_api: 'none'
+lastfm_api_key: 'none'
+
+# Database
+database_url: "postgres://scnbvhdzoozfgg:037e74101fa6b48ee717df7236d0e97818c7f1fe680202dfe03e30efd0fd20ab@ec2-54-146-91-153.compute-1.amazonaws.com:5432/d6667qf5tttnal"
+
+# Permission
+sudo_users: [71265226]
+support_users: [71265226]
+whitelist_users: [71265226]
